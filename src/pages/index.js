03d849917ebede400293ff3d199649c2f0e3af09
Mike McGowan <mike.mcgowan@peoplefluent.com>
2018-11-21 18:11:00 +0000

    Load vocabulary CSV and render it as a <ul>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,45 @@
-import React from "react"
+import React from 'react'
+import parse from 'csv-parse'
 
-export default () => <div>Hello, Gatsby vocab search!</div>
+import {tail, withIndex} from '../utils/misc'
+
+const url = 'https://raw.githubusercontent.com/mikemcgowan/memrise-scraper-scala/master/memrise_database.csv'
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      vocab: [],
+    }
+  }
+
+  async componentDidMount() {
+    const rawVocab = await fetch(url).then(
+      response => response.text()
+    )
+
+    parse(rawVocab, (err, output) => {
+      this.setState({
+        vocab: tail(output).map(withIndex)
+      })
+    })
+  }
+
+  render() {
+    const { vocab } = this.state
+    return (
+      <div>
+        {vocab.length === 0 ? (
+          <p>Loading vocab &hellip;</p>
+        ) : (
+          <ul>
+            {vocab.map(vocabItem => (
+              <li key={vocabItem[0]}>{vocabItem[0] + 1} {vocabItem[1]}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
---
+++ src/utils/misc.js
@@ -0,0 +1,2 @@
+export const tail = xs => xs.slice(1)
+export const withIndex = (item, index) => [index].concat(item)
