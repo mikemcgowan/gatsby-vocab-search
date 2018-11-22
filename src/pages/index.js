7eb7210976e3099399116cd73f2f624425e32475
Mike McGowan <mike.mcgowan@peoplefluent.com>
2018-11-22 16:29:59 +0000

    Reduced base font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from "../components/layout"
+
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
 export default ({ data }) => {
   const x = getRandomInt(data.allCsvItem.edges.length)
   return (
-    <div>
+    <Layout>
       <table>
         <thead>
           <tr>
             <th>Polish</th>
             <th>English</th>
-            <th>Other</th>
-            <th>Type</th>
           </tr>
         </thead>
         <tbody>
@@ -21,13 +21,11 @@ export default ({ data }) => {
             <tr key={index}>
               <td>{node.data[0]}</td>
               <td>{node.data[1]}</td>
-              <td>{node.data[2]}</td>
-              <td>{node.data[3]}</td>
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+    </Layout>
   )
 }
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,5 @@
 import Typography from 'typography'
 import theme from 'typography-theme-ocean-beach'
 
+theme.baseFontSize = '14px'
 export default new Typography(theme)
