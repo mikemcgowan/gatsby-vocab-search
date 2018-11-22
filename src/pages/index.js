3e21fe2e69a5e5ca6f81bc5f22fc3720eb93db42
Mike McGowan <mike.mcgowan@peoplefluent.com>
2018-11-22 15:56:27 +0000

    Render 10 random items rather than all items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-export default ({ data }) => (
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Polish</th>
-          <th>English</th>
-          <th>Other</th>
-          <th>Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.allCsvItem.edges.map(({ node }, index) => (
-          <tr key={index}>
-            <td>{node.data[0]}</td>
-            <td>{node.data[1]}</td>
-            <td>{node.data[2]}</td>
-            <td>{node.data[3]}</td>
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
+
+export default ({ data }) => {
+  const x = getRandomInt(data.allCsvItem.edges.length)
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Polish</th>
+            <th>English</th>
+            <th>Other</th>
+            <th>Type</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-)
+        </thead>
+        <tbody>
+          {data.allCsvItem.edges.slice(x, x + 10).map(({ node }, index) => (
+            <tr key={index}>
+              <td>{node.data[0]}</td>
+              <td>{node.data[1]}</td>
+              <td>{node.data[2]}</td>
+              <td>{node.data[3]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 export const query = graphql`
   query {
